1062fb08e9cce2166303d1c3ab6fcbe3909ee6cc
Lê Bình Phương <69758987+BinhPhuongIT@users.noreply.github.com>
2022-05-30 17:52:06 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # setting up new mode
-xrandr --newmode “1360x768_60.00″ 96.77 1152 1224 1344 1536 864 865
-868 900 -HSync +Vsync
-xrandr --addmode DVI-1 1360x768_60.00
-xrandr --addmode HDMI-1 1360x768_60.00
-xrandr --output DVI-1 --mode 1360x768_60.00
-xrandr --output HDMI-1 --mode 1360x768_60.00
+Xrandr
+gtf 1360 768 60
+xrandr --newmode "1360x768_60.00"  84.72  1360 1424 1568 1776  768 769 772 795  -HSync +Vsync
+xrandr --addmode VGA-1 1360x768_60.00
+xrandr --output VGA-1 --mode 1360x768_60.00
 ##sleep 1s
 ##done
 # https://ubunlog.com/vi/th%C3%AAm-%C4%91%E1%BB%99-ph%C3%A2n-gi%E1%BA%A3i-m%C3%A0n-h%C3%ACnh-t%C3%B9y-ch%E1%BB%89nh-ubuntu/
